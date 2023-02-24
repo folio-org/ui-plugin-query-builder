e9e017e55589d655a1d7d732d1f0fcfa03f7b473
Vlad Samets <uladislau_samets@epam.com>
2023-02-24 11:22:37 +0600

    lint

--- src/QueryBuilder/QueryBuilder/QueryBuilderModal/QueryBuilderModal.test.js
+++ src/QueryBuilder/QueryBuilder/QueryBuilderModal/QueryBuilderModal.test.js
@@ -15,7 +15,7 @@ const renderQueryBuilderModal = (
   <QueryBuilderModal
     setIsModalShown={setIsModalShown}
     isOpen={isOpen}
-  />
+  />,
 );
 
 describe('QueryBuilderModal', () => {
@@ -86,67 +86,4 @@ describe('QueryBuilderModal', () => {
 
     expect(rows.length).toEqual(1);
   });
-
-  // describe('Render accordion and titles', () => {
-  //   it('should render accordion if accordionHeadline prop is present', async () => {
-  //     render(renderResultViewer());
-
-  //     expect(screen.getByTestId('results-viewer-accordion')).toBeVisible();
-  //   });
-
-  //   it('should not render accordion if accordionHeadline prop is NULL', async () => {
-  //     render(renderResultViewer({ accordionHeadline: null }));
-
-  //     expect(screen.queryByTestId('results-viewer-accordion')).toBeNull();
-  //   });
-
-  //   it('should render subtitle with correct count of records', async () => {
-  //     render(renderResultViewer());
-
-  //     await waitFor(() => {
-  //       expect(screen.queryByText('Loading')).not.toBeInTheDocument();
-
-  //       expect(screen.getByText(`${content.totalElements} records found`)).toBeVisible();
-  //     });
-  //   });
-  // });
-
-  // describe('Initial and visible columns setters', () => {
-  //   it('should be called only once', async () => {
-  //     render(renderResultViewer());
-
-  //     await waitFor(() => {
-  //       expect(screen.queryByText('Loading')).not.toBeInTheDocument();
-
-  //       expect(setVisibleColumns).toHaveBeenCalledTimes(1);
-  //       expect(setColumns).toHaveBeenCalledTimes(1);
-  //     });
-  //   });
-  // });
-
-  // describe('Records table', () => {
-  //   it('should be rendered with pagination', async () => {
-  //     render(renderResultViewer());
-
-  //     await waitFor(() => {
-  //       expect(screen.queryByText('Loading')).not.toBeInTheDocument();
-
-  //       expect(screen.getByTestId('results-viewer-table')).toBeVisible();
-  //       expect(screen.getByText('Pagination')).toBeVisible();
-  //     });
-  //   });
-  // });
-
-  // describe('In progress state', () => {
-  //   it('Should render in progress when ', async () => {
-  //     const inProgressTitle = 'title';
-
-  //     render(renderResultViewer({
-  //       isInProgress: true,
-  //       inProgressTitle,
-  //     }));
-
-  //     expect(screen.getByText(inProgressTitle)).toBeVisible();
-  //   });
-  // });
 });
