8e15a75288d0fcab38a3fb18182888a266cd2e3d
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-03-09 12:12:28 +0400

    UIPQB-5: Field-action-value component: String

--- src/QueryBuilder/QueryBuilder/helpers/valueBuilder.js
+++ src/QueryBuilder/QueryBuilder/helpers/valueBuilder.js
@@ -3,10 +3,18 @@ import { DATA_TYPES } from '../constants/dataTypes';
 
 export const valueBuilder = (value, field) => {
   const dataType = fieldOptions.find(o => o.value === field).dataType || DATA_TYPES.BooleanType;
-  // TODO: add additional templates for dataTypes
+  // add additional templates for dataTypes
   const valueMap = {
     [DATA_TYPES.StringType]: () => `"${value}"`,
+    [DATA_TYPES.IntegerType]: () => value,
     [DATA_TYPES.BooleanType]: () => value,
+    [DATA_TYPES.StringType]: () => value,
+    [DATA_TYPES.RangedUUIDType]: () => value,
+    [DATA_TYPES.DateType]: () => value,
+    [DATA_TYPES.ArrayType]: () => value,
+    [DATA_TYPES.ObjectType]: () => value,
+    [DATA_TYPES.OpenUUIDType]: () => value,
+    [DATA_TYPES.EnumType]: () => value,
   };
 
   return valueMap[dataType]();
