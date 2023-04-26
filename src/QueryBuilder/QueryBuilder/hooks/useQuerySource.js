b12cf4e289d6892695f648ef88dae20bd4d01e47
vashjs <vadym.shchekotilin@gmail.com>
2023-04-26 22:23:36 +0300

    change useQuerySource logic

--- src/QueryBuilder/QueryBuilder/hooks/useQuerySource.js
+++ src/QueryBuilder/QueryBuilder/hooks/useQuerySource.js
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { getQueryStr, isQueryValid, mongoQueryToSource, sourceToMongoQuery } from '../helpers/query';
 import { booleanOptions, getFieldOptions, sourceTemplate } from '../helpers/selectOptions';
 
 export const useQuerySource = (initialValues, entityType) => {
-  const sourceInitialValue = initialValues
-    ? mongoQueryToSource({
-      mongoQuery: initialValues,
-      fieldOptions: getFieldOptions(entityType),
-      booleanOptions,
-    })
-    : [sourceTemplate(entityType)];
+  const sourceInitialValue = useMemo(() => {
+    return initialValues
+      ? mongoQueryToSource({
+        mongoQuery: initialValues,
+        fieldOptions: getFieldOptions(entityType),
+        booleanOptions,
+      })
+      : [sourceTemplate(entityType)];
+  }, [initialValues, entityType]);
 
   const [source, setSource] = useState(sourceInitialValue);
+
+  useEffect(() => { setSource(sourceInitialValue); }, [sourceInitialValue]);
 
   const queryStr = getQueryStr(source);
   const isQueryFilled = isQueryValid(source);
