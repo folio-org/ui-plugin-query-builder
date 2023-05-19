d05d4474dd15857a3ec887672a3236a560239dda
vashjs <vadym.shchekotilin@gmail.com>
2023-05-19 14:21:00 +0300

    fix value builder

--- src/QueryBuilder/QueryBuilder/helpers/valueBuilder.js
+++ src/QueryBuilder/QueryBuilder/helpers/valueBuilder.js
@@ -7,56 +7,47 @@ export const getCommaSeparatedStr = (arr) => {
   return arr?.map(el => `"${el.value}"`).join(',');
 };
 
+const getQuotedStr = (value) => {
+  return `"${value}"`;
+};
+
+const getFormattedDate = (value) => {
+  const date = moment(value);
+
+  date.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+
+  return (value ? `"${date.format(ISO_FORMAT)}"` : '');
+};
+
+const getFormattedUUID = (value, isInRelatedOperator) => {
+  return isInRelatedOperator
+    ? `"${value.replace(/,\s?/g, '","')}"`
+    : getQuotedStr(value);
+};
+
 export const valueBuilder = ({ value, field, operator, fieldOptions }) => {
   const dataType = fieldOptions?.find(o => o.value === field)?.dataType || DATA_TYPES.BooleanType;
   const isInRelatedOperator = [OPERATORS.IN, OPERATORS.NOT_IN].includes(operator);
+  const isArray = Array.isArray(value);
   // add additional templates for dataTypes
   const valueMap = {
-    [DATA_TYPES.StringType]: () => `"${value}"`,
+    [DATA_TYPES.StringType]: () => (isArray ? getCommaSeparatedStr(value) : getQuotedStr(value)),
 
-    [DATA_TYPES.IntegerType]: () => value,
+    [DATA_TYPES.IntegerType]: () => (isArray ? getCommaSeparatedStr(value) : value),
 
-    [DATA_TYPES.BooleanType]: () => `"${value}"`,
+    [DATA_TYPES.RangedUUIDType]: () => getQuotedStr(value),
 
-    [DATA_TYPES.ObjectType]: () => `"${value}"`,
+    [DATA_TYPES.ArrayType]: () => getCommaSeparatedStr(value),
 
-    [DATA_TYPES.RangedUUIDType]: () => {
-      if (Array.isArray(value)) {
-        return `(${getCommaSeparatedStr(value)})`;
-      }
+    [DATA_TYPES.EnumType]: () => getCommaSeparatedStr(value),
 
-      return `"${value}"`;
-    },
+    [DATA_TYPES.BooleanType]: () => getQuotedStr(value),
 
-    [DATA_TYPES.DateType]: () => {
-      const date = moment(value);
+    [DATA_TYPES.ObjectType]: () => getQuotedStr(value),
 
-      date.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+    [DATA_TYPES.DateType]: () => getFormattedDate(value),
 
-      return (value ? `"${date.format(ISO_FORMAT)}"` : '');
-    },
-
-    [DATA_TYPES.ArrayType]: () => {
-      if (Array.isArray(value) && isInRelatedOperator) {
-        return `(${getCommaSeparatedStr(value)})`;
-      }
-
-      return `"${value}"`;
-    },
-
-    [DATA_TYPES.OpenUUIDType]: () => (
-      isInRelatedOperator
-        ? `"${value.replace(/,\s?/g, '","')}"`
-        : `"${value}"`
-    ),
-
-    [DATA_TYPES.EnumType]: () => {
-      if (Array.isArray(value) && isInRelatedOperator) {
-        return `(${getCommaSeparatedStr(value)})`;
-      }
-
-      return `"${value}"`;
-    },
+    [DATA_TYPES.OpenUUIDType]: () => getFormattedUUID(value, isInRelatedOperator),
   };
 
   return valueMap[dataType]?.();
