9246ea8c82c6d13a8d93d3630732d0a1b312f464
vashjs <vadym.shchekotilin@gmail.com>
2024-02-19 18:43:55 +0100

    add support for not contains

--- src/QueryBuilder/QueryBuilder/QueryBuilderModal/DataTypeInput/DataTypeInput.js
+++ src/QueryBuilder/QueryBuilder/QueryBuilderModal/DataTypeInput/DataTypeInput.js
@@ -28,7 +28,7 @@ export const DataTypeInput = ({
 }) => {
   const isInRelatedOperator = [OPERATORS.IN, OPERATORS.NOT_IN].includes(operator);
   const isEqualRelatedOperator = [OPERATORS.EQUAL, OPERATORS.NOT_EQUAL].includes(operator);
-  const isContainsOperator = operator === OPERATORS.CONTAINS;
+  const isContainsRelatedOperator = [OPERATORS.CONTAINS, OPERATORS.NOT_CONTAINS].includes(operator);
   const hasSourceOrValues = source || availableValues;
 
   const textControl = ({ testId, type = 'text', textClass }) => {
@@ -169,7 +169,7 @@ export const DataTypeInput = ({
       return openUUIDTypeControls();
 
     case DATA_TYPES.ArrayType:
-      return isContainsOperator && hasSourceOrValues
+      return isContainsRelatedOperator && hasSourceOrValues
         ? selectControl({ testId: 'data-input-select-array' })
         : textControl({ testId: 'data-input-text-arrayType' });
 
