2c63c3afde17b55ab18a797f613c93017d02c10c
Noah Overcash <novercash@ebsco.com>
2024-02-22 17:48:09 -0500

    Add tests

---
+++ src/QueryBuilder/QueryBuilder/helpers/upgradeInitialValues.test.js
@@ -0,0 +1,37 @@
+import upgradeInitialValues from './upgradeInitialValues';
+
+describe('initial values legacy conversion', () => {
+  const ENTITY_TYPE = { columns: [{ name: 'foo', idColumnName: 'idColumn' }] };
+
+  it.each([
+    [null, null],
+    [null, undefined],
+    [undefined, null],
+    [undefined, undefined],
+    [null, {}],
+    [undefined, {}],
+    [{}, null],
+    [{}, undefined],
+  ])('ignores initialValues=%s and entityType=%s', (initialValues, entityType) => {
+    expect(upgradeInitialValues(initialValues, entityType)).toBe(initialValues);
+  });
+
+  it.each([{}, { foo: '' }, { bar: '' }, { foo: '', bar: '' }])(
+    'processes but does not convert non-id columns in %s',
+    (values) => {
+      expect(upgradeInitialValues(values, ENTITY_TYPE)).toStrictEqual(values);
+      // indicates that processing was actually done
+      expect(upgradeInitialValues(values, ENTITY_TYPE)).not.toBe(values);
+    },
+  );
+
+  it.each([
+    [{ idColumn: '' }, { foo: '' }],
+    [
+      { idColumn: '', bar: '' },
+      { foo: '', bar: '' },
+    ],
+  ])('converts %s to %s', (input, expected) => {
+    expect(upgradeInitialValues(input, ENTITY_TYPE)).toStrictEqual(expected);
+  });
+});
