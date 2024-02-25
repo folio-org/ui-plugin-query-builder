f1537bccc2816559c4b7b9fab00265bf6461fa0b
vashjs <vadym.shchekotilin@gmail.com>
2024-02-25 19:27:51 +0100

    UIPQB-75 Display grouped fields within a list row

---
+++ src/QueryBuilder/ResultViewer/DynamicTable/DynamicTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './DynamicTable.css';
+
+export const DynamicTable = ({ properties, values }) => {
+  const tableBodyRows = JSON.parse(values);
+
+  return (
+    <table className={css.DynamicTable}>
+      <thead>
+        <tr>{properties?.map((cell) => (
+          <th key={cell.property} style={{ width: `${100 / properties.length}%` }}>{cell.labelAlias}</th>
+        ))}
+        </tr>
+      </thead>
+      <tbody>
+        {tableBodyRows.map((row, index) => (
+          <tr key={index}>
+            {properties?.map((cell) => (
+              <td key={cell.property}>
+                {row[cell.property]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+DynamicTable.propTypes = {
+  properties: PropTypes.arrayOf(PropTypes.object),
+  values: PropTypes.string,
+};
