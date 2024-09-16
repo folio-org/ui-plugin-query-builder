74ed05772f282c50d5d271e219680da29fe22702
Vasilev Sergey <ed66679@gmail.com>
2024-09-16 09:58:17 +0400

    Fix lint

--- src/hooks/useLastNotEmptyValue.js
+++ src/hooks/useLastNotEmptyValue.js
@@ -4,7 +4,7 @@ import { isEmpty, isNumber } from 'lodash';
 export const useLastNotEmptyValue = (value, defaultValue) => {
   const [lastNotEmptyValue, setLastNotEmptyValue] = useState(defaultValue);
 
-  const isNotEmptyObject = !isEmpty(value)
+  const isNotEmptyObject = !isEmpty(value);
   const isPositiveNumber = isNumber(value) && value > 0;
 
   useEffect(() => {
