216ea505f76ee8b7c8bb375ddd734dcf5931b168
Noah Overcash <novercash@ebsco.com>
2024-09-20 12:03:27 -0400

    fix test

--- src/QueryBuilder/ResultViewer/DynamicTable/DynamicTable.test.js
+++ src/QueryBuilder/ResultViewer/DynamicTable/DynamicTable.test.js
@@ -46,17 +46,17 @@ describe('DynamicTable component', () => {
     },
   ];
 
-  const values =
-    '[{"code": "STATE-MONOSER", "value": 100.0, "fundId": "bbd4a5e1-c9f3-44b9-bfdf-d184e04f0ba0", "encumbrance": "eb506834-6c70-4239-8d1a-6414a5b08010", "distributionType": "percentage"}]';
-
   it.each(['[]', 'null', 'undefined', undefined, null])(
     'renders null value for empty/invalid input %s',
-    () => {
-      const { container } = render(<DynamicTable properties={properties} values="[]" />);
+    (v) => {
+      const { container } = render(<DynamicTable properties={properties} values={v} />);
 
       expect(container).toBeEmptyDOMElement();
     },
   );
+
+  const values =
+    '[{"code": "STATE-MONOSER", "value": 100.0, "fundId": "bbd4a5e1-c9f3-44b9-bfdf-d184e04f0ba0", "encumbrance": "eb506834-6c70-4239-8d1a-6414a5b08010", "distributionType": "percentage"}]';
 
   it('renders table with correct properties and values', () => {
     const { getByText } = render(<DynamicTable properties={properties} values={values} />);
