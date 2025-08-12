0caf09bd68081acb21d124e770a312aad2f76b39
Noah Overcash <novercash@ebsco.com>
2025-08-12 16:33:47 -0400

    translations

--- src/hooks/useDataOptions.js
+++ src/hooks/useDataOptions.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ORGANIZATIONS_TYPES } from '../constants/dataTypes';
 
 function getUniqueValues(a, b) {
