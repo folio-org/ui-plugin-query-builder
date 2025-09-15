a09c12bee924ea1675d3c9fade30a869678295f2
Matt Weaver <mweaver@ebsco.com>
2025-09-15 14:16:21 -0500

    Checkpoint from VS Code for coding agent session

---
+++ output/bundle.index01de90132ebd215d39fd.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunk_folio_plugin_query_builder=self.webpackChunk_folio_plugin_query_builder||[]).push([[57],{},l=>{var s=u=>l(l.s=u);l.O(0,[15],()=>s(18181));var e=l.O()}]);
+
+//# sourceMappingURL=bundle.index01de90132ebd215d39fd.js.map
